f55e12658b26dd9d073e7449f987b448a1e2d19e
Kevin Quail <kevinq.7365@gmail.com>
2023-10-20 13:00:41 -0500

    added tailwind config for styling

--- shipping_ships/src/components/nav/NavBar.jsx
+++ shipping_ships/src/components/nav/NavBar.jsx
@@ -2,19 +2,29 @@ import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <ul>
-      <li>
-        <Link to="/">HomePage</Link>
-      </li>
-      <li>
-        <Link to="/shippingships">Ships</Link>
-      </li>
-      <li>
-        <Link to="/haulingships">Haulers</Link>
-      </li>
-      <li>
-        <Link to="/docks">Docks</Link>
-      </li>
-    </ul>
+    <div className="flex justify-center bg-slate-400 rounded">
+      <ul className="bg-blue-700 p-4 rounded-lg flex justify-around items-center bg-slate-600 border-4 border-white rounded-3xl p-2 w-11/12">
+        <li>
+          <Link to="/" className="text-white">
+            HomePage
+          </Link>
+        </li>
+        <li>
+          <Link to="/shippingships" className="text-white">
+            Ships
+          </Link>
+        </li>
+        <li>
+          <Link to="/haulingships" className="text-white">
+            Haulers
+          </Link>
+        </li>
+        <li>
+          <Link to="/docks" className="text-white">
+            Docks
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 };
